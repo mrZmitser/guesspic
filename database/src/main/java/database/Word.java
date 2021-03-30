5f2f66938f74ac1a03419afce68701e19d7a3854
vladilyushchenko <vladislavilyushchenko@gmail.com>
2021-03-30 20:50:01 +0300

    application context added, Word class is being geterating from context

--- database/src/main/java/database/Word.java
+++ database/src/main/java/database/Word.java
@@ -2,10 +2,11 @@ package database;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 
-
+@Component("word")
 @Entity
 @Table(name = "words")
 public class Word {
--- spring_server/src/main/java/spring_server/WordController.java
+++ spring_server/src/main/java/spring_server/WordController.java
@@ -1,6 +1,8 @@
 package spring_server;
 
 import database.Word;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +12,11 @@ import java.util.Random;
 
 @RestController
 public class WordController {
-    private static List<Word> words;
+    @Autowired
+    private ApplicationContext appContext;
 
     @GetMapping("/word")
-    public String word() throws SQLException {
-        if (words == null) {
-            words = database.HibernateRequest.getAllTable();
-        }
-        return generateWord();
-    }
-
-    private static String generateWord() {
-        return words.get(new Random().nextInt(words.size())).getWord();
+    public Word word() throws SQLException {
+        return appContext.getBean(Word.class, "word");
     }
 }
