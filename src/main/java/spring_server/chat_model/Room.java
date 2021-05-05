da19056bb020afb3a3beb594a58b4dc171508010
vladilyushchenko <vladislavilyushchenko@gmail.com>
2021-05-06 00:10:27 +0300

    maybe final version of server

---
+++ src/main/java/spring_server/chat_model/Room.java
@@ -0,0 +1,76 @@
+package spring_server.chat_model;
+
+import database.Word;
+import lombok.Getter;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Room {
+    @Getter
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+
+    private final ConcurrentLinkedQueue<User> paintersQueue = new ConcurrentLinkedQueue<>();
+
+    @Getter
+    private final String name;
+    @Getter
+    private final Long id;
+    @Getter
+    private int painterId = 1;
+    @Getter
+    private final int bonus = 5;
+
+    private static long idCounter = 1;
+
+    @Getter
+    private Optional<Word> currWord = Optional.empty();
+
+    public Room(String chatRoomName) {
+        this.name = chatRoomName;
+        this.id = idCounter++;
+    }
+
+    public boolean addMessage(Message message) {
+        if (currWord.isPresent() && message.getContent().equalsIgnoreCase(currWord.get().getWord())) {
+            users.get(message.getSenderId()).addScore(bonus);
+            updateWordAndPainter();
+            return true;
+        }
+        return false;
+    }
+
+    public User getUserById(int id) {
+        return users.get(id);
+    }
+
+    public void addUser(User user) {
+        paintersQueue.add(user);
+        this.users.put(user.getId(), user);
+
+        if (users.size() == 2) {
+            painterId = user.getId();
+            updateWordAndPainter();
+        }
+    }
+
+    public void removeUser(int userId) {
+        paintersQueue.remove(getUserById(userId));
+        users.remove(userId);
+    }
+
+    // TO MAKE BETTER
+    private void updateWordAndPainter() {
+        if (paintersQueue.isEmpty()) {
+            paintersQueue.addAll(users.values());
+        }
+
+        users.get(painterId).setPainter(false);
+        painterId = paintersQueue.poll().getId();
+        users.get(painterId).setPainter(true);
+
+        //currWord = WordDao.getRandWord();
+        currWord = Optional.of(new Word("Rap"));
+    }
+}
--- src/main/java/spring_server/controllers/WebSocketChatController.java
+++ src/main/java/spring_server/controllers/WebSocketChatController.java
@@ -1,32 +1,45 @@
 package spring_server.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import spring_server.chat_model.GameChatController;
-import spring_server.chat_model.GameChatMessage;
+import spring_server.chat_model.*;
 
 @Controller
 public class WebSocketChatController {
     @Autowired
-    GameChatController gameChatController;
+    private SimpMessageSendingOperations messagingTemplate;
+    @Autowired
+    private RoomsController roomsController;
 
-    @MessageMapping("/chat.send") //chat-example/app/chat.send
-    @SendTo("/topic/public") //chat-example/topic/public
-    public GameChatMessage sendMessage(@Payload GameChatMessage chatMessage) {
-        gameChatController.getRoom(chatMessage.getChatRoomId()).addMessage(chatMessage);
-        return chatMessage;
-    }
+    private static final Gson gson = new GsonBuilder().create();
 
-    @MessageMapping("/chat.newUser")
+    @MessageMapping("/chat.send")
     @SendTo("/topic/public")
-    public GameChatMessage newUser(@Payload GameChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-        gameChatController.getRoom(chatMessage.getChatRoomId()).addMessage(chatMessage);
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSenderId());
+    public Message sendMessage(String jsonMessage) {
+        Message chatMessage = gson.fromJson(jsonMessage, Message.class);
+        Room room = roomsController.getRoomById(chatMessage.getChatRoomId());
+
+        if (chatMessage.getType() == MessageType.DISCONNECT) {
+            roomsController.getRoomById(chatMessage.getChatRoomId()).removeUser(chatMessage.getSenderId());
+        }
+
+        if (room.addMessage(chatMessage)) {
+            messagingTemplate.convertAndSend("/topic/public",
+                    Message.builder().
+                    type(MessageType.WIN).
+                    content("{\"winnerId\":\"" + chatMessage.getSenderId() + "\", \"bonus\":\"" + room.getBonus() +
+                            "\", \"nextPainterId\":\"" + room.getPainterId() + "\"}").
+                    senderId(chatMessage.getSenderId()).
+                    chatRoomId(room.getId()).
+                    build()
+            );
+        }
+
         return chatMessage;
     }
-
 }
